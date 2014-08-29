3ad1a2ae79a02b74ff0f20b996df519a3737f5e1
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-29 19:36:19 +1200

    orbits

---
+++ orbits.py
@@ -0,0 +1,44 @@
+from pylab import *
+
+for v0 in [0.2]:#linspace(0.5, 1.5, 101):
+	# Initial position
+	pos = array([1., 0.])
+
+	# Initial velocity
+	vel = array([0., v0])
+
+	# Timestep
+	dt = 0.001
+	tfinal = 10.
+	steps = int(tfinal/dt)
+
+	keep = empty((steps, 2))
+
+	ion()
+	for i in xrange(0, steps):
+	  phi1 = arctan2(pos[1], pos[0])
+
+	  pos += 0.5*dt*vel
+	  accel = -pos/(pos[0]**2 + pos[1]**2)**1.5
+	  vel += dt*accel
+	  pos += 0.5*dt*vel
+	  keep[i, :] = pos
+
+	  phi2 = arctan2(pos[1], pos[0])
+
+	  if i%50 == 0:
+	    hold(False)
+	    plot(keep[0:(i+1), 0], keep[0:(i+1), 1], 'bo')
+	    hold(True)
+	    plot(0, 0, 'r*')
+	    axis('equal')
+	    draw()
+
+	  if phi1 < 0. and phi2 > 0.:
+	    keep = keep[0:(i+1), :]
+	    break
+	 
+	savetxt('keep' + str(v0) + '.txt', keep)
+ioff()
+show()
+
