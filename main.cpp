feeaaa3ae41bac7a5b338a1698f9201a8b363d63
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-31 15:24:51 +1200

    Copied fake data from RJObject

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "Lookup.h"
+#include "Start.h"
+#include "MyModel.h"
+#include "Data.h"
 
 using namespace std;
+using namespace DNest3;
 
-int main()
+int main(int argc, char** argv)
 {
+	// Load the data
+	Data::get_instance().load("fake_data.txt");
+
 	Lookup::get_instance().load();
 
+	MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
+	sampler.run();
 
 	return 0;
 }
