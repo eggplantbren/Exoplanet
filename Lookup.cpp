b6dbc1b5094d4d4a3d97ce0f4a0b6779b87328bf
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-31 15:03:31 +1200

    Loads orbits

--- Lookup.cpp
+++ Lookup.cpp
@@ -1,5 +1,7 @@
 #include "Lookup.h"
 #include <string>
+#include <sstream>
+#include <iostream>
 
 using namespace std;
 
@@ -16,10 +18,17 @@ void Lookup::load()
 	double v0;
 	Orbit o;
 	string filename;
+
 	for(int i=0; i<121; i++)
 	{
 		v0 = 0.4 + 0.005*i;
-//		filename = string("Orbits/orbit" + string())
+		stringstream ss(stringstream::out);
+		ss.precision(3);
+		ss<<fixed<<v0;
+		filename = string("Orbits/orbits" + ss.str() + ".dat");
+
+		o.load(filename.c_str());
+		orbits.push_back(o);
 	}
 }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "Lookup.h"
 
 using namespace std;
 
 int main()
 {
+	Lookup l;
+	l.load();
+
 	return 0;
 }
 
